fb0a774485a00742ac599049a780a5f902b3d7e3
void_17 <61356189+void2012@users.noreply.github.com>
2026-03-02 22:56:56 +0700

    Revert "fix: fix resolution detection (#133)" (#143)
    This reverts commit afa4efbc8513cbb8565b9700cae48648e3dffe29.

--- Minecraft.Client/Windows64/Windows64_Minecraft.cpp
+++ Minecraft.Client/Windows64/Windows64_Minecraft.cpp
@@ -4,7 +4,6 @@
 #include "stdafx.h"
 
 #include <assert.h>
-#include <ShellScalingApi.h>
 #include "GameConfig\Minecraft.spa.h"
 #include "..\MinecraftServer.h"
 #include "..\LocalPlayer.h"
@@ -717,9 +716,19 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+	/*
+	// Declare DPI awareness so GetSystemMetrics returns physical pixels
+	SetProcessDPIAware();
 	g_iScreenWidth = GetSystemMetrics(SM_CXSCREEN);
 	g_iScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	{
+		char buf[128];
+		sprintf(buf, "Screen resolution: %dx%d\n", g_iScreenWidth, g_iScreenHeight);
+		OutputDebugStringA(buf);
+	}
+	*/
+
 
 	if(lpCmdLine)
 	{
