01928dd23d082c7f0c01391254b4ff9005de50b9
LetsGoAway <68365423+letsgoawaydev@users.noreply.github.com>
2026-03-05 04:26:56 +0800

    Update ItemInHandRenderer.cpp (#418)

--- Minecraft.Client/ItemInHandRenderer.cpp
+++ Minecraft.Client/ItemInHandRenderer.cpp
@@ -548,7 +548,7 @@ void ItemInHandRenderer::render(float a)
         glPushMatrix();
         float d = 0.8f;
 
-#if defined __ORBIS__ || defined __PS3__
+#if defined __ORBIS__ || defined __PS3__ || defined _WINDOWS64
 		static const float swingPowFactor = 1.0f;
 #else
 		static const float swingPowFactor = 4.0f;		// 4J added, to slow the swing down when nearest the player for avoiding luminance flash issues
