4b3322401dc3127a608fe1c2c6fd8afafaff8ae6
eh-K <165239265+eh-K@users.noreply.github.com>
2026-03-07 22:31:20 -0600

    Adjust gamma calculation based on slider value (#893)

--- Minecraft.Client/GameRenderer.cpp
+++ Minecraft.Client/GameRenderer.cpp
@@ -945,9 +945,9 @@ float GameRenderer::ComputeGammaFromSlider(float slider0to100)
     slider = min(slider, 100.0f);
 
     if (slider > 50.0f)
-        return 1.0f + (slider - 50.0f) / 50.0f * 0.5f; // 1.0 -> 1.5
+        return 1.0f + (slider - 50.0f) / 50.0f * 1.2f; // 1.0 -> 1.5
     else
-        return 1.0f - (50.0f - slider) / 50.0f * 0.5f; // 1.0 -> 0.5
+        return 1.0f - (50.0f - slider) / 50.0f * 0.4f; // 1.0 -> 0.5
 }
 
 void GameRenderer::CachePlayerGammas()
