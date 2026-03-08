90b9460a3e0054a622c58a0d7d471fbba758ec3f
Alezito2008 <92759854+Alezito2008@users.noreply.github.com>
2026-03-08 05:02:23 -0300

    fix: prevent TNT minecarts from exploding if TNT host option is disabled (#921)

--- Minecraft.World/MinecartTNT.cpp
+++ Minecraft.World/MinecartTNT.cpp
@@ -80,9 +80,12 @@ void MinecartTNT::explode(double speedSqr)
 	if (!level->isClientSide)
 	{
 		double speed = sqrt(speedSqr);
-		if (speed > 5) speed = 5;
-		level->explode(shared_from_this(), x, y, z, (float) (4 + random->nextDouble() * 1.5f * speed), true);
-		remove();
+		if (speed > 5.0) speed = 5.0;
+		if (app.GetGameHostOption(eGameHostOption_TNT))
+		{
+			level->explode(shared_from_this(), x, y, z, (float) (4 + random->nextDouble() * 1.5f * speed), true);
+			remove();
+		}
 	}
 }
 
