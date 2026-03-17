16dc6f925b46c5cd91ac1ca1ba6caf90706a037b
rtm516 <rtm516@users.noreply.github.com>
2026-03-17 19:22:24 +0000

    Change KEY_HOST_SETTINGS back to to VK_TAB

--- Minecraft.Client/Windows64/KeyboardMouseInput.h
+++ Minecraft.Client/Windows64/KeyboardMouseInput.h
@@ -34,7 +34,7 @@ public:
 	static const int KEY_DEBUG_MENU = VK_F4;
 	static const int KEY_THIRD_PERSON = VK_F5;
 	static const int KEY_DEBUG_CONSOLE = VK_F6;
-	static const int KEY_HOST_SETTINGS = VK_F8;
+	static const int KEY_HOST_SETTINGS = VK_TAB;
 	static const int KEY_FULLSCREEN = VK_F11;
 
 	// todo: implement and shi
