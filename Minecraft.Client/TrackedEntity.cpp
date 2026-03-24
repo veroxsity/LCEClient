93b389cb70b2067d32735ba637349ebc1708c2bf
Sylvessa <225480449+sylvessa@users.noreply.github.com>
2026-03-24 04:04:07 -0500

    Fix XUID logging (#1395)
    * pass invalid_xuid to other players
    
    * actually more simple fix

--- Minecraft.Client/TrackedEntity.cpp
+++ Minecraft.Client/TrackedEntity.cpp
@@ -653,11 +653,12 @@ shared_ptr<Packet> TrackedEntity::getAddEntityPacket()
 
 		PlayerUID xuid = INVALID_XUID;
 		PlayerUID OnlineXuid = INVALID_XUID;
-		if( player != nullptr )
-		{
-			xuid = player->getXuid();
-			OnlineXuid = player->getOnlineXuid();
-		}
+		// do not pass xuid/onlinxuid to cleints
+		//if( player != nullptr )
+		//{
+		//	xuid = player->getXuid();
+		//	OnlineXuid = player->getOnlineXuid();
+		//}
 		// 4J Added yHeadRotp param to fix #102563 - TU12: Content: Gameplay: When one of the Players is idle for a few minutes his head turns 180 degrees.
 		return std::make_shared<AddPlayerPacket>(player, xuid, OnlineXuid, xp, yp, zp, yRotp, xRotp, yHeadRotp);
 	}
