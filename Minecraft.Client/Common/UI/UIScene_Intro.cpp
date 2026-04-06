a1dbd663970c67ff1c9f1a4ddf929573f758dd66
veroxsity <veroxsity@gmail.com>
2026-04-06 23:34:02 +0100

    Add _LINUX64 case for platformIdx in UIScene_Intro.cpp

--- Minecraft.Client/Common/UI/UIScene_Intro.cpp
+++ Minecraft.Client/Common/UI/UIScene_Intro.cpp
@@ -34,6 +34,8 @@ UIScene_Intro::UIScene_Intro(int iPad, void *initData, UILayer *parentLayer) : U
 	int platformIdx = 4;
 #elif defined(__PSVITA__)
 	int platformIdx = 5;
+#elif defined(_LINUX64)
+	int platformIdx = 0; // Use same as Windows64 for now
 #endif
 
 	IggyDataValue result;
